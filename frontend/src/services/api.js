34c771d04d53aea4b6650fdfdff3843d09cea91f
Omar Mohamed Fahem <145774215+OmaR-WezA@users.noreply.github.com>
2026-01-03 10:20:20 +0200

    Update api.js
    https://shawnee-uncommutable-royal.ngrok-free.dev/ 
    /api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://shawnee-uncommutable-royal.ngrok-free.dev/',
   headers: {
     'Content-Type': 'application/json',
   },
